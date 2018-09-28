cfda089f582b8e9ac19ce0bb415de3af7fefd531
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-09-28 18:04:15 +0200

    finish

--- OptionNull/Program.cs
+++ OptionNull/Program.cs
@@ -11,8 +11,6 @@ namespace OptionNull
             var user2 = new User("Kate", new PersonalInfo("221 B Baker St, London, England"));
 
             Console.WriteLine("User: {1}, address is: {0}", user1.Info.Reduce(new PersonalInfo("default")).Address, user1.Name);
-
-            
             Console.WriteLine("User: {1}, address is: {0}", user2.Info.Reduce(new PersonalInfo("default")).Address, user2.Name);
         }
     }
